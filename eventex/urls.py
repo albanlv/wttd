e1363f7d0f9ad0cdf1eaec9799c3633862bd71ce
albanlv <albanlv@gmail.com>
2013-12-08 22:33:44 +0100

    Subscription

--- eventex/urls.py
+++ eventex/urls.py
@@ -5,5 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'eventex.core.views.home', name='home'),
+    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
 )
